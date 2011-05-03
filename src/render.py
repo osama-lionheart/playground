1fc328a9eab1b95d34ab8b9b0a6e5f76031a4a85
Osama Ibrahim <osama.lionheart@gmail.com>
2011-05-03 04:07:49 +0200

    changing the query string to text instead of plain

--- src/render.py
+++ src/render.py
@@ -2,9 +2,9 @@ from docutils.core import publish_string
 import cherrypy
 
 class Render(object):
-    def index(self, plain='Hello, world!\n================='):
+    def index(self, text='Hello, world!\n================='):
         return publish_string(
-                              source=plain,
+                              source=text,
                               settings_overrides={'file_insertion_enabled': 0, 'raw_enabled': 0},
                               writer_name='html')
     index.exposed = True
