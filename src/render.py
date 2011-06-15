402c6905ccc7acbfef40fde665b42d1b6c44533f
Osama Ibrahim <osama.lionheart@gmail.com>
2011-06-16 00:52:17 +0200

    Testing Mako Templates

--- src/render.py
+++ src/render.py
@@ -1,11 +1,13 @@
 from docutils import core
 from docutils.core import publish_string
 import cherrypy
+from mako.template import Template
 
 class Render(object):
     def index(self, text='Hello, world!\n================='):
         parts = core.publish_parts(
                                    source=text,
                                    writer_name='html')
-        return parts['body_pre_docinfo']+parts['fragment']
+        
+        return Template("hello ${data}!").render(data="world") + parts['body_pre_docinfo']+parts['fragment']
     index.exposed = True
